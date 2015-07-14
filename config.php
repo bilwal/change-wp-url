407198538362e27ca6d78dfc2e482d3130011bae
Robin Hossain <robinhossainbd@gmail.com>
2015-07-14 14:56:49 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	
+	$servername = "localhost"; // your server name
+	$dbname = "wp"; // your databse name
+	$username = "robin"; // your database username
+	$password = "robin"; // your database password
+	$db_prefix = "wp_"; // your wordpress db prefix.
+	
+	
+	
+?>
